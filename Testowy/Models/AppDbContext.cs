fda1f9fb51ce968fb2e9bcb6029a8170444cad4d
Piotr Szczepański <szczepan1795@gmail.com>
2020-01-13 15:59:07 +0100

    update

---
+++ Testowy/Models/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace Testowy.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        {
+        }
+    }
+}
