82f81d6d44907443480e3e31f4ad6dbab2f9b823
Piotr Szczepański <szczepan1795@gmail.com>
2020-01-15 14:29:21 +0100

    Aktualizacja

---
+++ Testowy/Controllers/OpiniaController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Testowy.Models;
+
+namespace Testowy.Controllers
+{
+    public class OpiniaController : Controller
+    {
+        private readonly IOpiniaRepository opiniaRepository;
+
+        public OpiniaController(IOpiniaRepository opiniaRepository)
+        {
+            this.opiniaRepository = opiniaRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Index(Opinia opinia)
+        {
+            if (ModelState.IsValid)
+            {
+                opiniaRepository.DodajOpinie(opinia);
+                return RedirectToAction("OpiniaWyslana");
+            }
+            return View(opinia);
+        }
+
+        public IActionResult OpiniaWyslana()
+        {
+            return View();
+        }
+    }
+}
--- Testowy/Models/Opinia.cs
+++ Testowy/Models/Opinia.cs
@@ -1,10 +1,21 @@
-﻿namespace Testowy.Models
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace Testowy.Models
 {
     public class Opinia
     {
+        [BindNever]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Nazwa użytkownika jest wymagana")]
+        [StringLength(100, ErrorMessage = "Nazwa użytkownika jest za długa")]
         public string NazwaUżytkownika { get; set; }
+        [Required(ErrorMessage = "Adres email jest wymagany")]
+        [StringLength(100, ErrorMessage = "Adres email jest za długi")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Wiadomość jest wymagana")]
+        [StringLength(5000, ErrorMessage = "Wiadomość jest za długa")]
         public string Wiadomosc { get; set; }
         public bool OczekujeOdpowiedzi { get; set; }
     }
